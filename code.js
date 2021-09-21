07119d10998846bfe05e072993f124595e24bc9b
Jimmy Muthengi <theeoligarchy@gmail.com>
2021-09-21 10:07:08 +0300

    first commit

---
+++ code.js
@@ -0,0 +1,24 @@
+
+var button = document.getElementById('check');
+
+button.addEventListener('click', function() {
+
+
+    var value1 = document.getElementById('valueA').value;
+    var value2 = document.getElementById('valueB').value;
+    var value3 = document.getElementById('valueC').value;
+    var result = document.getElementById('result');
+    result.style.color = 'white'
+
+    if(value1 === value2 && value2 === value3 && value3 === value1) {
+        result.innerHTML = 'This is an Equilateral Triangle'
+    }
+
+    else if ( value1 === value2 && value2 !== value3 && value1) {
+        result.innerHTML = 'This is an Isoscles Triangle'
+    }
+
+    else {
+        result.innerHTML = 'This is a scalene triangle'
+    }
+})
